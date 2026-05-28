14f65551a8f626249c323782a6bdb509bae61014
kk121308741-rgb <kk121308741@gmail.com>
2026-05-28 15:47:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'xuetian_community.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
